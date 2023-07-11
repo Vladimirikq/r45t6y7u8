e743926e57bfaf070693e1d2e7464b8f3f939cb2
Vladimirikq <104771122+Vladimirikq@users.noreply.github.com>
2023-07-11 08:51:15 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Button(text='Hello, Android!')
+
+if __name__ == '__main__':
+    MyApp().run()
